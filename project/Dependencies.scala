89634d6eb3966aba14e641da9d229c4d47db4ff5
Yaroslav Klymko <t3hnar@gmail.com>
2018-03-22 15:05:40 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,20 +3,20 @@ import sbt._
 object Dependencies {
 
   object Akka {
-    private val version = "2.5.9"
+    private val version = "2.5.11"
     val Actor        = "com.typesafe.akka" %% "akka-actor" % version
     val ClusterTools = "com.typesafe.akka" %% "akka-cluster-tools" % version
     val Testkit      = "com.typesafe.akka" %% "akka-testkit" % version
     val Stream       = "com.typesafe.akka" %% "akka-stream" % version
   }
 
-  val safeAkka = "com.evolutiongaming" %% "safe-actor" % "1.4"
+  val safeAkka = "com.evolutiongaming" %% "safe-actor" % "1.6"
 
   val scalax = "com.github.t3hnar" %% "scalax" % "3.3"
 
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.8.0"
 
-  val nel = "com.evolutiongaming" %% "nel" % "1.0"
+  val nel = "com.evolutiongaming" %% "nel" % "1.1"
 
   val metricTools = "com.evolutiongaming" %% "metric-tools" % "1.1"
 
