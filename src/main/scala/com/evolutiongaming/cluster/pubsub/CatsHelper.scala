d50d52ebd513e94b9e5563634c4097a1ace78903
mr-git <8796159+mr-git@users.noreply.github.com>
2019-09-12 16:04:10 +0300

    Scala 2.12.10 and 2.13.0, SBT 1.3.0, Cats 2.0.0

--- src/main/scala/com/evolutiongaming/cluster/pubsub/CatsHelper.scala
+++ src/main/scala/com/evolutiongaming/cluster/pubsub/CatsHelper.scala
@@ -1,15 +1,16 @@
 package com.evolutiongaming.cluster.pubsub
 
-import cats.{Applicative, Foldable, Monoid, Parallel}
+import cats.{Applicative, Foldable, Monoid, Parallel, ~>}
 
 object CatsHelper {
 
   implicit class ParallelOps(val self: Parallel.type) extends AnyVal {
 
-    def foldMap[T[_] : Foldable, M[_], F[_], A, B: Monoid](ta: T[A])(f: A => M[B])(implicit P: Parallel[M, F]): M[B] = {
-      implicit val applicative = P.applicative
-      implicit val monoid = Applicative.monoid[F, B]
-      val fb = Foldable[T].foldMap(ta)(f.andThen(P.parallel.apply))
+    def foldMap[T[_] : Foldable, M[_], A, B: Monoid](ta: T[A])(f: A => M[B])(implicit P: Parallel[M]): M[B] = {
+      val applicative: Applicative[P.F] = P.applicative
+      val monoid: Monoid[P.F[B]] = Applicative.monoid[P.F, B](applicative, Monoid[B])
+      val parallel: M ~> P.F = P.parallel
+      val fb = Foldable[T].foldMap(ta)(f.andThen(parallel.apply(_)))(monoid)
       P.sequential(fb)
     }
   }
@@ -17,7 +18,7 @@ object CatsHelper {
 
   implicit class TOps[T[_], A](val self: T[A]) extends AnyVal {
 
-    def parFoldMap[M[_], F[_], B: Monoid](f: A => M[B])(implicit F: Foldable[T], P: Parallel[M, F]): M[B] = {
+    def parFoldMap[M[_], B: Monoid](f: A => M[B])(implicit F: Foldable[T], P: Parallel[M]): M[B] = {
       Parallel.foldMap(self)(f)
     }
   }
