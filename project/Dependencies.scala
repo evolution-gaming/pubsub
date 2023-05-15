610a12017a46ad5e2d0912a022917a3ba0c0b16f
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2023-05-15 18:14:46 +0200

    bump pureconfig to 0.17.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val `safe-actor`         = "com.evolutiongaming" %% "safe-actor"         % "3.0.0"
   val `akka-serialization` = "com.evolutiongaming" %% "akka-serialization" % "1.0.4"
   val nel                  = "com.evolutiongaming" %% "nel"                % "1.3.4"
-  val `metric-tools`       = "com.evolutiongaming" %% "metric-tools"       % "1.2.6"
+  val `metric-tools`       = "com.evolutiongaming" %% "metric-tools"       % "2.0.0"
   val `cats-helper`        = "com.evolutiongaming" %% "cats-helper"        % "2.7.6"
   val scache               = "com.evolutiongaming" %% "scache"             % "3.2.2"
   val scalatest            = "org.scalatest"       %% "scalatest"          % "3.2.3"
