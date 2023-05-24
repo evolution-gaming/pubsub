19c36f915adcab3ea30f211044dd60cb9a46f39c
Scala Steward <scala_steward@virtuslab.com>
2023-05-24 20:04:12 +0000

    Update cats-helper to 3.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val `akka-serialization` = "com.evolutiongaming" %% "akka-serialization" % "1.0.4"
   val nel                  = "com.evolutiongaming" %% "nel"                % "1.3.4"
   val `metric-tools`       = "com.evolutiongaming" %% "metric-tools"       % "2.0.0"
-  val `cats-helper`        = "com.evolutiongaming" %% "cats-helper"        % "3.0.1"
+  val `cats-helper`        = "com.evolutiongaming" %% "cats-helper"        % "3.0.3"
   val scache               = "com.evolutiongaming" %% "scache"             % "4.0.0"
   val scalatest            = "org.scalatest"       %% "scalatest"          % "3.2.3"
   val scalax               = "com.github.t3hnar"   %% "scalax"             % "3.8.1"
